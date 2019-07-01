f082d9e9bc3f94bde7a87b530a88271dcc34cf80
sanjeev8386 <sanjeev18@navgurukul.org>
2019-07-01 16:48:59 +0530

    published raw

---
+++ server.js
@@ -0,0 +1,28 @@
+var dotenv  = require("dotenv")
+dotenv.config();
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.user,
+    pass: process.env.pass
+  }
+});
+
+var mailOptions = {
+  from: process.env.user,
+  to: 'sanjeevku1411@gmail.com',
+  cc: 'noreplytosanjeev@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy! I enjoyed Very much'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
